9f2e60ad3b1c9ce94dc02250a239e0e431ebfbab
Hans Georg ZOJER <e0226374@student.tuwien.ac.at>
2014-07-20 10:14:38 +0200

    ImageUtil part 1

---
+++ src/at/hgz/picturetrainer/img/ImageUtil.java
@@ -0,0 +1,40 @@
+package at.hgz.picturetrainer.img;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import at.hgz.picturetrainer.R;
+
+public final class ImageUtil {
+	
+	private static ImageUtil instance;
+
+	private Context context;
+	
+	private ImageUtil(Context context) {
+		this.context = context;
+	}
+	
+	public static ImageUtil getInstance(Context context) {
+		if (instance == null) {
+			instance = new ImageUtil(context);
+		}
+		return instance;
+	}
+	
+	public byte[] getResourceImage(String filename) {
+		try {
+			Resources res = context.getResources();
+			int id = res.getIdentifier(filename, "raw", context.getPackageName());
+			InputStream in = res.openRawResource(R.raw.default_dictionaries);
+			byte[] picture = IOUtils.toByteArray(in);
+			return picture;
+    	} catch (Exception e) {
+    		throw new RuntimeException(e.getMessage(), e);
+    	}
+	}
+	
+}
