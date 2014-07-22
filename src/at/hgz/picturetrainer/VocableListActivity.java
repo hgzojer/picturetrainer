3da4308b092e87079409c1ab4261f6d7f1ac18bf
Hans Georg ZOJER <e0226374@student.tuwien.ac.at>
2014-07-22 20:43:19 +0200

    fixed config

--- src/at/hgz/picturetrainer/VocableListActivity.java
+++ src/at/hgz/picturetrainer/VocableListActivity.java
@@ -1,9 +1,6 @@
 package at.hgz.picturetrainer;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
